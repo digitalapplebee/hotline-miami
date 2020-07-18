823b1bb6a48d257937fdca902337bd82bf32728a
Dmitry Priborovich <digitalapplebee@gmail.com>
2020-07-19 00:32:23 +0300

    Add Text component

---
+++ src/components/Text/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Text() {
+  return (
+    <div className="text">
+      <p>
+        Now, for you two
+        <br />
+        ...
+      </p>
+      <p>
+        You're supposed to be killing people!
+        <br />
+        Make it look like you really hit them.
+      </p>
+      <p>
+        ...
+        <br />
+        Press any key...
+      </p>
+    </div>
+  );
+}
+
+export default Text;
